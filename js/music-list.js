c9f2870b3db7b5ee606fe42026675113c2a29d80
Adoniscrak <121782226+Adoniscrak@users.noreply.github.com>
2023-09-26 22:13:09 -0400

    Update music-list.js

--- js/music-list.js
+++ js/music-list.js
@@ -11,46 +11,21 @@
 
 let allMusic = [
   {
-    name: "Harley Bird - Home",
-    artist: "Jordan Schor",
-    img: "music-1",
-    src: "music-1"
+    name: "(no copyright music) chill type beat “imagine” _ prod. by lukrembo",
+    artist: "LuKremBo",
+    img: "imagine",
+    src: "(no copyright music) chill type beat “imagine” _ prod. by lukrembo"
   },
   {
-    name: "Ikson Anywhere – Ikson",
-    artist: "Audio Library",
-    img: "music-2",
-    src: "music-2"
+    name: "RDjavi x El Canelilla - La Para De La Ciudad (Video Oficial)",
+    artist: "RDjavi",
+    img: "la para de la ciudad",
+    src: "RDjavi x El Canelilla - La Para De La Ciudad (Video Oficial)"
   },
   {
-    name: "Beauz & Jvna - Crazy",
-    artist: "Beauz & Jvna",
-    img: "music-3",
-    src: "music-3"
+    name: "Redchinawave",
+    artist: "Redchinawave",
+    img: "Redchinawave",
+    src: "Redchinawave"
   },
-  {
-    name: "Hardwind - Want Me",
-    artist: "Mike Archangelo",
-    img: "music-4",
-    src: "music-4"
-  },
-  {
-    name: "Jim - Sun Goes Down",
-    artist: "Jim Yosef x Roy",
-    img: "music-5",
-    src: "music-5"
-  },
-  {
-    name: "Lost Sky - Vision NCS",
-    artist: "NCS Release",
-    img: "music-6",
-    src: "music-6"
-  },
-  // like this paste it and remember to give comma after ending of this bracket }
-  // {
-  //   name: "Here is the music name",
-  //   artist: "Here is the artist name",
-  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
-  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
-  // }
 ];
